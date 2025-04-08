ad7242b31784635a50362698e3bbfaabd109c083
fiebsy <feebs@whop.com>
2025-04-08 14:26:01 -0500

    Migrate portfolio to v1-portfolio path and create simple home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,7 @@
-'use client';
-
-import Hero from '@/components/hero-section';
-import Works from '@/components/works-section';
-import Skills from '@/components/skills-section';
-
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-17">
-      <div className="max-w-xl mx-auto px-4 mt-[60px]">
-        <Hero />
-        <Works />
-        <div className=" flex-col hidden gap-8">
-          <Skills />
-        </div>
-      </div>
+    <main className="min-h-screen bg-white flex items-center justify-center">
+      <h1 className="text-3xl font-semibold">Hello World</h1>
     </main>
   );
 }
---
+++ src/app/v1-portfolio/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import Hero from '@/components/hero-section';
+import Works from '@/components/works-section';
+import Skills from '@/components/skills-section';
+
+export default function Portfolio() {
+  return (
+    <main className="min-h-screen bg-gray-17">
+      <div className="max-w-xl mx-auto px-4 mt-[60px]">
+        <Hero />
+        <Works />
+        <div className=" flex-col hidden gap-8">
+          <Skills />
+        </div>
+      </div>
+    </main>
+  );
+}
