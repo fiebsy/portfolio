8c615b1af7821bf4ca8e275209117c77ee4857e9
fiebsy <feebs@whop.com>
2025-04-18 12:38:08 -0500

    swipe gestures

--- src/components/works/index.tsx
+++ src/components/works/index.tsx
@@ -214,10 +214,9 @@ export function Works() {
             <Sheet.View
               className="sheet-view"
               contentPlacement="bottom"
-              detents="max(env(safe-area-inset-bottom, 0px) + 46px, 500px)"
               swipeTrap={true}
               swipeOvershoot={false}
-              swipeDismissal={true}
+              swipeDismissal={false}
               swipe={true}
             >
               <Sheet.Backdrop
